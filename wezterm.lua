a54e61d73d163c44de7fce1879125b103fbd7645
Sankar Shanmugam <1890648+srgsanky@users.noreply.github.com>
2024-08-23 02:07:29 -0700

    Remove bottom padding

--- wezterm.lua
+++ wezterm.lua
@@ -112,7 +112,7 @@ config.window_padding = {
 	left = "1cell",
 	right = "1cell",
 	top = "0.5cell",
-	bottom = "0.5cell",
+	bottom = "0cell",
 }
 
 -- https://wezfurlong.org/wezterm/scrollback.html
